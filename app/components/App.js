762dfb99af14932d0b5bdeae25efd7788d5b157d
Ashiya Banu <ashiya1005@gmail.com>
2017-08-30 15:25:19 +1000

    Added main container

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,27 @@
 var React = require('react');
+var Weather = require('./Weather');
+var Time = require('./Time');
+var Greeting = require('./Greeting');
 
 class App extends React.Component {
   render() {
     return (
-      <div>Hello world</div>
+      <div className='container'>
+        <div className={'space-between'}>
+          <div>Links</div>
+          <Weather />
+        </div>
+        <div>
+          <Time />
+          <Greeting />
+          <div>Todo</div>
+        </div>
+        <div className='space-between'>
+          <div>Settings</div>
+          <div>Quote</div>
+          <div>Todo List</div>
+        </div>
+      </div>
     )
   }
 }
